6ecb7672cde4e8a144815bd7d3b611735a70f3a3
HM\CHAMP <christoffer.snallman@hm.com>
2020-10-12 14:47:49 +0200

    Created structure for hangman

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 namespace Hangman
 {
@@ -6,7 +9,120 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World2!");
+            string wordToGuess = "Applepie";
+            //CharSet wordToGuessSet = "a"
+
+           List<string> correctlyGuessed = new List<string>();
+           List<string> incorrectlyGuessed = new List<string>();
+
+            
+            int numberOfTries = 10;
+
+            //while (numberOfTries != 0)
+            // {
+            //if (correctlyGuessed == correctlyGuessedSet)
+            //{
+            //    Console.WriteLine("You Won!");
+            //    CurrentWord(wordToGuess);
+            //    break;
+            //}
+
+            //#prints the word with "-" except for the letters the user guessed correctly.
+            CurrentWord(wordToGuess);
+
+            //#prints a list of the letters the user guessed incorrectly.
+            Console.WriteLine(incorrectlyGuessed);
+
+            Console.WriteLine($"Number of guesses left: {numberOfTries}");
+
+            Console.WriteLine("Please guess a letter.");
+            var answer = Console.Read();
+
+            //if (answer present in wordToGuessSet) {
+            //add answer to correctlyGuessed;
+            //}
+
+            //else {
+            //numberOfTries--;
+            //add answer to incorrectlyGuessed;
+            //}
+
+
+            // }
+            //if (numberOfTries == 0) {
+            //Console.WriteLine($"You lost, the right word was {wordToGuess}");
+
+
+        }
+        static void CurrentWord(string word) {
+            foreach (char letter in word)
+            {
+                if (letter //is present in correctlyGuessed)
+                Console.WriteLine(letter);
+
+                else
+                {
+                    Console.WriteLine("-");
+                }
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Exampleword = Applepie
+//Examplewordset = Exampleword into set of letters
+
+//listOfCorrectlyGuessedLetters = []
+//listOfIncorrectlyGuessedLetters = []
+
+//numberOfTries = 10
+
+//while numberOfTries != 0 {
+
+//if listOfCorrectlyGuessedLetters == examplewordset
+// print "You WON!"
+// print currentWord
+// break;
+
+//Print currentWord
+//For every letter in wordToGuess,
+//if letter is present in listOfCorrectlyGuessedLetters - print letter
+//else print "-"
+
+//print listOfIncorrectlyGuessedLetters
+
+//print $"Guesses left {numberOfTries}"
+
+// //first time should give us "_ _ _ _ _ _ _ _ "
+
+//ask for a single letter
+
+//if letter is present in Examplewordset
+//add letter to listOfCorrectlyGuessedLetters
+
+//else
+//  numberOfTries--
+// letter is added to listOfIncorrectlyGuessedLetters
+//}
+
+// if number of tries == 0 {
+//print "you lost :( "
