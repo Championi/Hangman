a18f5d42325839b1379ad5dd5847c7e5efb4a4e2
HM\FRBUL <fredrik.bullsimonsen@hm.com>
2020-10-14 13:26:46 +0200

    Added test code

---
+++ HangMan.Core.Test2/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HangMan.Core.Test2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void should_return_Bla()
+        {
+
+        }
+    }
+}
---
+++ Hangman.Core.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hangman.Core.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
